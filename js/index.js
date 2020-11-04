ae1bb1fd7fd4766bd3a78a3ce94e8f159f07a815
Elijah <elijahoneal@yahoo.com>
2020-11-03 20:08:48 -0500

    10 effects

--- js/index.js
+++ js/index.js
@@ -26,17 +26,31 @@ nav.forEach( link => {
     link.addEventListener( 'mouseout' , event => event.target.style.fontSize = '2em');
 })
 
+let intro = document.querySelector('.intro');
+intro.addEventListener('click' , event => event.target.style.backgroundColor = 'red');
+
+// Header IMG
+
 let mainImg = document.querySelector('.intro img');
-mainImg.addEventListener('wheel' , event => event.target.style.width = '70%');
+mainImg.addEventListener('click' , event => event.target.style.width = '70%').stopPropagation();
+
+
 
 let h2 = document.querySelectorAll('h2');
 h2.forEach( title => {
-    window.addEventListener('keydown' , event => title.style.fontSize = '12px');
 
     window.addEventListener('keydown' , event => title.style.backgroundColor= 'cyan');
 
-    window.addEventListener('keyup' , event => title.style.fontSize = '36px');
+    window.addEventListener('wheel' , event => title.style.fontSize = '36px');
 
     window.addEventListener('keyup' , event => title.style.backgroundColor= 'yellow');
 
+
+})
+
+window.addEventListener('load' , event => alert('Magic School Bus!'));
+
+let paragraphs = document.querySelectorAll('p');
+paragraphs.forEach( item => {
+  window.addEventListener( 'resize' , event => item.style.fontWeight = 900);
 })
